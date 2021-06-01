e07f9c559b4e35987791c5dee04ff2e37cc21647
Alex Keri <akeri@ucvts.tec.nj.us>
2021-06-01 10:34:08 -0400

    deleted comment

--- Bishop.java
+++ Bishop.java
@@ -1,4 +1,4 @@
-public class Bishop implements ChessPiece { //TODO: check if king is in check
+public class Bishop implements ChessPiece {
     private int rank = -1;
     private int file = -1;
     private char color = 'n';
